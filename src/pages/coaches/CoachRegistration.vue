451bafeedb8cdf96f1422a740890d917bdfa5a0f
hosseinmiri02 <hossein.miri1531@gmail.com>
2024-01-19 14:18:16 +0330

    initial commit

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section>
+      <base-card>
+        <h2>Register as a coach now!</h2>
+        <coach-form @save-data="saveData"></coach-form>
+      </base-card>
+    </section>
+  </div>
+</template>
+
+<script>
+import CoachForm from "../../components/coaches/CoachForm.vue";
+
+export default {
+  components: {
+    CoachForm,
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch("coaches/registerCoach", data);
+      this.$router.replace("/coaches");
+    },
+  },
+};
+</script>
